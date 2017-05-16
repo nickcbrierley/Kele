a638be637e8b61f6d29ee4a3be161096d4c6a4ce
Nick Brierley <nickcbrierley@gmail.com>
2017-05-16 18:18:39 +0000

    checkpoint4 mentor availability completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,6 +13,11 @@ class Kele
     
     def get_me
         response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => @auth_token })
-        @parse_user = JSON.parse(response.body)
+        @me = JSON.parse(response.body)
+    end
+    
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("#{@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+        @mentor_availability = JSON.parse(response.body)
     end
 end
